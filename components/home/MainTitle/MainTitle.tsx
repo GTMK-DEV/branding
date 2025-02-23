ab4997ecad1fa4548346f5a46e0a7ed8c2b8f98c
hoyyChoi <hoyeon8165@gmail.com>
2025-02-23 13:50:38 +0900

    feat: initial commit

--- components/home/MainTitle/MainTitle.tsx
+++ components/home/MainTitle/MainTitle.tsx
@@ -17,7 +17,7 @@ export default function MainTitle() {
         함께 성장하는 공간
       </span>
       <span className={styles.sub_text}>
-        라이톤에서 팀 회고를 작성하고 관리하세요.
+        빌리브에서 팀 회고를 작성하고 관리하세요.
         <br />
         그리고 팀에 맞춤화 된 프라이빗한 공간을 누리세요.
       </span>
